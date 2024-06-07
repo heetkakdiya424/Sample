1a851ee5ea3f1ee82fb7f172c8c163fb336938fd
Heet <heetkakdiya567@gmail.com>
2024-06-07 12:35:57 +0530

    commit

---
+++ includes/footer.php
@@ -0,0 +1,9 @@
+            <div class="hk-footer-wrap container">
+                <footer class="footer">
+                    <div class="row">
+                        <div class="col-md-6 col-sm-12">
+<p>Created by 6 sem student</p>
+                        </div>
+                    </div>
+                </footer>
+            </div>
